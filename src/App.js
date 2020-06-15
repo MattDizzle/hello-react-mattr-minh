306bf89f01c437d6b52e1cd64866673bdd1ae68c
Matthew Rougely <matthewrougelysr@gmail.com>
2020-06-15 14:23:25 -0700

    Added components and CSS styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header'
+import Bio from './Bio';
+import Education from './Education';
+import Work from './Work';
+import Footer from './Footer'
 
 function App() {
+  const name = "Blake";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+      <Header />
+      <Bio />
+      <Education />
+      <Work />
+      <Footer />
+      </main>
     </div>
   );
 }
